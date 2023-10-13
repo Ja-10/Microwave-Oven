d4fb165451422abddb9bedd258dce0bb8bccf9a6
Ja-10 <147098779+Ja-10@users.noreply.github.com>
2023-10-14 00:06:39 +0530

    Update clcd.c

--- clcd.c
+++ clcd.c
@@ -1,29 +1,27 @@
 #include <xc.h>
 #include "clcd.h"
 
-void clcd_write(unsigned char byte, unsigned char mode)  // byte -> 'A', 1
+void clcd_write(unsigned char byte, unsigned char mode)  
 {
     CLCD_RS = mode;
     
-    CLCD_DATA_PORT = byte & 0xF0;   // 0x41 & 0xF0 : 0x40 : 0100 0000 (RD7 to RD4)
+    CLCD_DATA_PORT = byte & 0xF0;   
     CLCD_EN = HI;
     __delay_us(100);
     CLCD_EN = LOW;
 
-    CLCD_DATA_PORT = (byte & 0x0F) << 4; // 0x41 & 0x0F : 0000 0001 << 4 = PORTD 
+    CLCD_DATA_PORT = (byte & 0x0F) << 4; 
     CLCD_EN = HI;
     __delay_us(100);
     CLCD_EN = LOW;
     
-    __delay_us(4100); // 4.1msec
+    __delay_us(4100); 
 }
 
 static void init_display_controller(void)
 {
-    /* Startup Time for the CLCD controller */
     __delay_ms(30);
     
-    /* The CLCD Startup Sequence */
     clcd_write(EIGHT_BIT_MODE, INST_MODE);
     __delay_us(4100);
     clcd_write(EIGHT_BIT_MODE, INST_MODE);
@@ -43,10 +41,8 @@ static void init_display_controller(void)
 
 void init_clcd(void)
 {
-    /* Setting the CLCD Data Port as Output */
     CLCD_DATA_PORT_DDR = 0x00;
     
-    /* Setting the RS and EN lines as Output */
     CLCD_RS_DDR = 0;
     CLCD_EN_DDR = 0;
     
