1ccacbc9a2617ce072e7e52b350d9ee716c3944c
Ja-10 <147098779+Ja-10@users.noreply.github.com>
2023-10-14 00:09:25 +0530

    Update matrix_keypad.c

--- matrix_keypad.c
+++ matrix_keypad.c
@@ -4,15 +4,11 @@
 
 void init_matrix_keypad(void)
 {
-    /* Setting the Columns as Inputs (RB2 - RB0)*/
-    MATRIX_KEYPAD_COLUMN_PORT_DDR = MATRIX_KEYPAD_COLUMN_PORT_DDR | 0x07; //TRISB = TRISB | 0x07;
-    /* Setting the Rows as Output (RD3 - RD0)*/
-    MATRIX_KEYPAD_ROW_PORT_DDR = MATRIX_KEYPAD_ROW_PORT_DDR & 0xF0; // TRISD = TRISD & 0xF0 -> (TRISD0 to TRISD3)
+    MATRIX_KEYPAD_COLUMN_PORT_DDR = MATRIX_KEYPAD_COLUMN_PORT_DDR | 0x07; 
+    MATRIX_KEYPAD_ROW_PORT_DDR = MATRIX_KEYPAD_ROW_PORT_DDR & 0xF0; 
     
-    /* Enabling PORTB Pullups */
     nRBPU = 0;
     
-    /* Making all the Rows HIGH to start with */
     ROW1 = HI;
     ROW2 = HI;
     ROW3 = HI;
@@ -111,7 +107,7 @@ unsigned char read_matrix_keypad(unsigned char mode)
     static unsigned char once = 1;
     unsigned char key;
     
-    key = scan_keypad(); //0 1 2 3 4 5 6 7 8 9  '*' '#'
+    key = scan_keypad(); 
     
     if (mode == LEVEL)
     {
